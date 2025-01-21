49a5d2edb8b31aa2192981679753ed3b42056bb0
laboratorio <laboratorio@UPOAULA10324>
2025-01-21 08:29:21 -0500

    Agregar archivos de proyecto.

---
+++ RobertoGuañaExamen_3P/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RobertoGuañaExamen_3P
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
