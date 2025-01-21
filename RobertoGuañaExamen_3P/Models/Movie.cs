8d3c496d907f391648f7cf94455b945f6c9ec151
RFelipeGR <robertofelipe20.rf@gmail.com>
2025-01-21 08:35:00 -0500

    creacion de models y codigo de services

---
+++ RobertoGuañaExamen_3P/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+
+namespace RobertoGuañaExamen_3P.Models
+
+{
+    public class Movie
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string MainActor { get; set; }
+        public string Awards { get; set; }
+        public string Website { get; set; }
+        public string Scordova { get; set; }
+    }
+}
--- RobertoGuañaExamen_3P/Service/MovieService.cs
+++ RobertoGuañaExamen_3P/Service/MovieService.cs
@@ -1,12 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Android.Graphics;
+using MovieSearchApp.Models;
+using Newtonsoft.Json;
 
-namespace RobertoGuañaExamen_3P.Service
+namespace RobertoGuañaExamen3P.Services
 {
-    internal class MovieService
+    public class MovieService
     {
+        private readonly HttpClient _httpClient = new();
+
+        public async Task<Movie?> SearchMovieAsync(string query)
+        {
+            try
+            {
+                var url = $"https://freetestapi.com/api/v1/movies?search={query}&limit=1";
+                var response = await _httpClient.GetStringAsync(url);
+
+                var result = JsonConvert.DeserializeObject<dynamic>(response);
+                if (result.movies != null && result.movies.Count > 0)
+                {
+                    var movieData = result.movies[0];
+                    return new Movie
+                    {
+                        Title = (string)movieData.title,
+                        Genre = (string)movieData.genres[0],
+                        MainActor = (string)movieData.actors[0],
+                        Awards = (string)movieData.awards,
+                        Website = (string)movieData.website,
+                        Scordova = "RGuaña"
+                    };
+                }
+
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
